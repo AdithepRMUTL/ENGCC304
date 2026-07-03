30c55df9408078299057ce8c327242f0c123e0ed
Adithep <adithep@MacBook-Air-khxng-Adithep.local>
2026-07-03 09:04:22 +0700

    Update all files

---
+++ 69543206042-1_ENGCC304_lab_2_10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float Score;
+    
+    scanf("%f", &Score);
+
+    printf("Score = %.2f\n", Score);
+
+    return 0;
+}
--- 69543206042-1_ENGCC304_lab_2_9.c
+++ 69543206042-1_ENGCC304_lab_2_9.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    
+    int price, quantity , Total_price;
+
+    scanf("%d %d", &price, &quantity);
+    Total_price = price * quantity;
+
+    printf("Total price = %d", Total_price);
+
+    return 0;
 }
